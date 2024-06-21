6ba09f3a0198ca86685a81b3a7dab5c114839426
zzy <qq905066802@gmail.com>
2024-06-21 17:21:15 +0800

    conmany test

--- Hello.java
+++ Hello.java
@@ -1,6 +1,15 @@
 public class Hello {
     public static void main(String[] args){
         System.out.println("Hello,world!");//文本输出“Hello，world！”
-
+        char a = 'A';
+        char zh = '中';
+        System.out.println(a);
+        System.out.println(zh);
+        int n = 100;
+        int m = 100;
+        n++;
+        ++m;
+        System.out.println(n);
+        System.out.println(m);
     }
 }
---
+++ Test.java
@@ -0,0 +1,7 @@
+/**
+ * @author zzylm
+ * @description TODO
+ * @date 2024-06-21 17:20
+ */
+public class Test {
+}
